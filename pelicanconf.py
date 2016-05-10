51857440ca0d005482eb3ca3efccc409b7a1ec2d
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2016-05-10 14:36:13 -0500

    Make CNAME file static

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,8 @@ DEFAULT_PAGINATION = 10
 DISPLAY_CATEGORIES_ON_MENU = False
 THEME = 'chunk'
 
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
+
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
