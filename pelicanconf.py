96075111ae558bff628988281a4010270fc27d08
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2016-05-19 14:17:24 -0500

    Fix RSS and Atom feeds

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,8 @@ TIMEZONE = 'Europe/Berlin'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
