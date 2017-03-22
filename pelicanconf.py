6ef2d6f3e37e3ddc22048a7486fc9b282228a629
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2017-03-22 12:59:33 +0100

    Add correct formatting to article template

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,12 +12,12 @@ PATH = 'content'
 TIMEZONE = 'Europe/Berlin'
 
 DEFAULT_LANG = 'en'
+DEFAULT_DATE_FORMAT = '%d %B %Y'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
